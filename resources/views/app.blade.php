bd5a1fd8c0ef99970ef9a0025bdcd5f33c559510
hindsadik <hindsadikchorouk@gmail.com>
2023-07-07 17:32:57 +0100

    first commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My App</title>
+    <!-- Ajoutez ici les liens vers vos fichiers CSS et JS -->
+</head>
+<body>
+    @inertia
+    <!-- Ajoutez ici le conteneur pour votre application React -->
+    <div id="app">
+        <h1>Hello Word</h1>
+    </div>
+</body>
+</html>
